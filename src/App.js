4a06dc137ad21ec5735ccdfc5bb5c00333b9905b
Sean McGillis <slcidevice@yahoo.com>
2022-02-07 20:44:10 -0700

    Added conditional renders for: no movies, is loading, and has movies states.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
-
+    setIsLoading(false);
     const transformedMovies = data.results.map((movieData) => {
       return {
         id: movieData.episode_id,
@@ -27,7 +29,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
